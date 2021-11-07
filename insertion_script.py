2c7e0e7aed2bb5f241de2f33f1e79bc0bd8f006e
Annicheez <60089829+Annicheez@users.noreply.github.com>
2021-11-07 21:58:54 +0000

    Update insertion_script.py

--- insertion_script.py
+++ insertion_script.py
@@ -11,7 +11,7 @@ files = [os.path.join(root, name) for root, direc, files in os.walk(parent_dir) 
     for name in files if name.split('.')[-1] == 'gz'] 
 
 # Initalise master dataframe to append to and injest into the RDS
-# Concactenating dataframes locally using pandas to ensure 0% data loss
+# Concactenate dataframes locally using pandas to ensure 0% data loss
 master_df = pd.DataFrame()
 
 for file in files:
@@ -19,7 +19,7 @@ for file in files:
     master_df = master_df.append(pd.read_json(file, lines=True))
 
 
-# Converting pandas object types to string for safe injestion of non-sql dtypes
+# Converting pandas object types to string for safe injestion of non-sql supported dtypes
 master_df = master_df.astype(
     {col: 'str' for col, dtype in master_df.dtypes.to_dict().items() if dtype == 'object'}
 )
@@ -36,7 +36,7 @@ db_connection_str = f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 con = create_engine(db_connection_str).connect()
 
-# Write master file to a table in the db
+# Write master df to a table in the db
 # Setting chunksize to increase insert more than one row in a single insert statement
 master_df.to_sql(con=con, name='tweets_data', 
                 if_exists='append', 
